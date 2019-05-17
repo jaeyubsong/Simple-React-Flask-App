278b5b36ae95d7f83f37315985291f4447b86a47
sungjune.p <egs060@gmail.com>
2019-05-17 17:46:39 +0900

    Implemented fileupload

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,11 +1,19 @@
 import os
 
-from flask import Flask, redirect, url_for, request, render_template, current_app, jsonify
+
+
+from flask import Flask, flash, session, redirect, url_for, request, render_template, current_app, jsonify
 from pymongo import MongoClient
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+from werkzeug.utils import secure_filename
 
 import json
 import logging
+
+
+UPLOAD_PATH = '../Simple-React-Flask-App/toUpload'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
@@ -21,7 +29,7 @@ def index():
   return render_template('index.html', items=items)
 
  
-@app.route('/new', methods=['POST'])
+@app.route('/new', methods=['POST', 'GET'])
 def new():
   item_doc = {
     'name': request.form['name'],
@@ -30,6 +38,15 @@ def new():
   db.tododb.insert_one(item_doc)
   print("Added something")
   return redirect(url_for('index'))
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+  name_to_delete = request.form['name_to_delete']
+  db.tododb.remove({"name": name_to_delete })
+  print("\nDelete Successfully\n")
+  return redirect('/')
+
 
 @app.route('/getData', methods=['POST'])
 def getData():
@@ -46,6 +63,15 @@ def getData():
   response.headers.add('Access-Control-Allow-Origin', '*')
   return response
 
+
+@app.route('/upload', methods=['GET', 'POST'])
+def fileUpload():
+  if request.method == 'POST':
+    toUpload = request.files['toUpload']
+    current_app.logger.info(toUpload.filename)
+    toUpload.save(secure_filename(toUpload.filename))
+    return 'upload successfully'
+  
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
